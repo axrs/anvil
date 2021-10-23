7156de5221d8f43528a48ad3874a69017649eaf2
Alexander Scott <xander@axrs.io>
2021-10-24 00:30:20 +1000

    Chore: Kindle the Forge

---
+++ anvil.ps1
@@ -0,0 +1,6 @@
+If(!(test-path '.dart_tool') -Or -not(Test-Path -Path 'pubspec.lock' -PathType Leaf))
+{
+    & dart.exe pub get | Out-Null
+}
+& dart.exe run chassis_forge:build --directory forge --main forge/main.dart --executable-target kernel --verbose | Out-Null
+& dart.exe run forge/main.dill @args
---
+++ anvil.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+if [ ! -d '.dart_tool' ] || [ ! -f 'pubspec.lock' ];then
+  dart pub get >/dev/null
+fi
+dart run chassis_forge:build --directory forge --main forge/main.dart --executable-target kernel --verbose >/dev/null
+# shellcheck disable=SC2068
+dart run forge/main.dill $@
