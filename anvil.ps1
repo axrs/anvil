5ae5ebf57f08c7d9b6a4fb846eeefb48b43e673c
Alexander Scott <xander@axrs.io>
2021-12-17 09:01:11 +1000

    Chore: Update Scripts

--- anvil.ps1
+++ anvil.ps1
@@ -1,5 +1,7 @@
-If (!(test-path '.dart_tool') -Or -not(Test-Path -Path 'pubspec.lock' -PathType Leaf)) {
-    & dart.exe pub get | Out-Null
+#!/usr/bin/env pwsh
+If(!(test-path '.dart_tool') -Or -not(Test-Path -Path 'pubspec.lock' -PathType Leaf))
+{
+    & dart pub get | Out-Null
 }
-& dart.exe run chassis_forge:build --directory tool --main tool/main.dart --executable-target kernel --verbose
-& dart.exe run tool/main.dill @args
+& dart run chassis_forge:build --directory tool --main tool/main.dart --executable-target kernel --verbose | Out-Null
+& dart run tool/main.dill @args
