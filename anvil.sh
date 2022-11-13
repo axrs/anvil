dee950bda6adacadea7b8bbaa33c5a90fee6bb2a
Alexander Scott <xander@axrs.io>
2022-11-13 19:19:12 +1000

    Tool: Remove dart tooling

--- anvil.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-if [ ! -d '.dart_tool' ] || [ ! -f 'pubspec.lock' ];then
-  dart pub get >/dev/null
-fi
-dart run chassis_forge:build --directory tool --main tool/main.dart --executable-target kernel --verbose >/dev/null
-# shellcheck disable=SC2068
-dart run tool/main.dill "$@"
