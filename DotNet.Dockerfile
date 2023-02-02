fc6eff6f4fe16971bc1e20642c4397e5ce9e6b7e
Alexander R Scott <xander@axrs.io>
2023-02-02 15:23:45 +1000

    Refactor: Remove legacy

--- DotNet.Dockerfile
+++
@@ -1,9 +0,0 @@
-ARG ANVIL_BASE_TAG
-FROM $ANVIL_BASE_TAG
-LABEL maintainer="Alexander Scott <xander@axrs.io>"
-LABEL description="A Docker Development Build and Test Container where my Projects are hammered into shape"
-SHELL ["/bin/bash", "-o", "pipefail", "-c"]
-
-ENV DOTNET_ROOT=/usr/dotnet
-ENV PATH="$DOTNET_ROOT:${PATH}"
-ARG DOTNET_VERSION
--- build.sh
+++ build.sh
@@ -1,113 +1,52 @@
 #!/usr/bin/env bash
 set -euo pipefail
-mkdir -p .docker_context
 
-# Holds a list of tags that were built for pushing at the end of the job
-built_tags=()
+docker images -a | grep "anvil" | awk '{print $3}' | xargs docker rmi --force || true
 
-declare -A dart_version
-dart_version=(["2"]="2.19.0-1" ["2.19"]="2.19.0-1" ["2.18"]="2.18.6-1")
-declare -A flutter_version
-flutter_version=(["3"]="3.7.0" ["3.7"]="3.7.0" ["3.3"]="3.3.10")
-declare -A flutter_dart_version
-flutter_dart_version=(["3.7.0"]="2.19.0-1" ["3.3.10"]="2.18.6-1")
-
-# Various SDK versions. Note Java 15/17 are defined below
-dart_version="2.19.0-1"
-flutter_version="3.7.0" # Note: Flutter versions are tied to dart versions
-clojure_version="1.11.1.1208"
-tag_prefix="axrs/anvil"
-
-base_tag="$tag_prefix:dart-$dart_version"
-
-echo "DART $dart_version"
-for version_tag in "2" "2.19" "$dart_version"; do
-	root_tag="$tag_prefix:dart-$version_tag"
-	docker build --pull \
-		--file Dart.Dockerfile \
-		--build-arg DART_VERSION=$dart_version \
-		--tag $root_tag \
-		.docker_context/
-	docker build \
-		--file Cloud.Dockerfile \
-		--build-arg ANVIL_BASE_TAG=$root_tag \
-		--tag "$root_tag-cloud" \
-		.docker_context/
-	built_tags+=("$root_tag" "$root_tag-cloud")
+# Build and tag new containers
+readarray -t combinations < <(jq -c '.[]' combinations.json)
+IFS=$'\n'
+for combination in "${combinations[@]}"; do
+	dart_version=$(jq -r '.dartVersion' <<<"${combination}")
+	java_version=$(jq -r '.javaVersion // empty' <<<"${combination}")
+	clojure_version=$(jq -r '.clojureVersion // empty' <<<"${combination}")
+	dotnet_version=$(jq -r '.dotnetVersion // empty' <<<"${combination}")
+	flutter_version=$(jq -r '.flutterVersion // empty' <<<"${combination}")
+	android_version=$(jq -r '.androidVersion // empty' <<<"${combination}")
+	android_build_tools_version=$(jq -r '.androidBuildToolsVersion // empty' <<<"${combination}")
+	readarray -t tags < <(jq -cr '.tags[]' <<<"${combination}")
+	for tag in "${tags[@]}"; do
+		echo "$tag"
+		docker build \
+			--pull \
+			--file Anvil.Dockerfile \
+			--build-arg DART_VERSION="$dart_version" \
+			--build-arg INCLUDE_CLOUD="false" \
+			--build-arg JAVA_VERSION="$java_version" \
+			--build-arg CLOJURE_VERSION="$clojure_version" \
+			--build-arg DOTNET_VERSION="$dotnet_version" \
+			--build-arg FLUTTER_VERSION="$flutter_version" \
+			--build-arg ANDROID_VERSION="$android_version" \
+			--build-arg ANDROID_BUILD_TOOLS_VERSION="$android_build_tools_version" \
+			--tag "axrs/anvil:$tag" \
+			scripts/
+		docker build \
+			--file Anvil.Dockerfile \
+			--build-arg DART_VERSION="$dart_version" \
+			--build-arg INCLUDE_CLOUD="true" \
+			--build-arg JAVA_VERSION="$java_version" \
+			--build-arg CLOJURE_VERSION="$clojure_version" \
+			--build-arg DOTNET_VERSION="$dotnet_version" \
+			--build-arg FLUTTER_VERSION="$flutter_version" \
+			--build-arg ANDROID_VERSION="$android_version" \
+			--build-arg ANDROID_BUILD_TOOLS_VERSION="$android_build_tools_version" \
+			--tag "axrs/anvil:$tag-cloud" \
+			scripts/
+	done
+	sleep 10
 done
+unset IFS
 
-dotnet_version="6.0.405"
-echo "DOTNET $dotnet_version from $base_tag"
-for version_tag in "6" "6.0" "$dotnet_version"; do
-	root_tag="$tag_prefix:dotnet-$version_tag"
-	docker build \
-		--file DotNet.Dockerfile \
-		--build-arg ANVIL_BASE_TAG=$base_tag \
-		--build-arg DOTNET_VERSION=$dotnet_version \
-		--tag $root_tag \
-		.docker_context/
-	docker build \
-		--file Cloud.Dockerfile \
-		--build-arg ANVIL_BASE_TAG=$root_tag \
-		--tag "$root_tag-cloud" \
-		.docker_context/
-	built_tags+=("$root_tag" "$root_tag-cloud")
-done
+# Push all tags
+docker push axrs/anvil --all-tags
 
-dotnet_version="7.0.102"
-echo "DOTNET $dotnet_version from $base_tag"
-for version_tag in "7" "7.0" "$dotnet_version"; do
-	root_tag="$tag_prefix:dotnet-$version_tag"
-	docker build \
-		--file DotNet.Dockerfile \
-		--build-arg ANVIL_BASE_TAG=$base_tag \
-		--build-arg DOTNET_VERSION=$dotnet_version \
-		--tag $root_tag \
-		.docker_context/
-	docker build \
-		--file Cloud.Dockerfile \
-		--build-arg ANVIL_BASE_TAG=$root_tag \
-		--tag "$root_tag-cloud" \
-		.docker_context/
-	built_tags+=("$root_tag" "$root_tag-cloud")
-done
-
-for version_tag in "11" "15" "17"; do
-	echo "JAVA $version_tag from $base_tag"
-	root_tag="$tag_prefix:java-$version_tag"
-	docker build \
-		--file Java.Dockerfile \
-		--build-arg ANVIL_BASE_TAG=$base_tag \
-		--build-arg CLOJURE_VERSION=$clojure_version \
-		--build-arg JAVA_VERSION=$version_tag \
-		--tag $root_tag \
-		.docker_context/
-	docker build \
-		--file Cloud.Dockerfile \
-		--build-arg ANVIL_BASE_TAG=$root_tag \
-		--tag "$root_tag-cloud" \
-		.docker_context/
-	built_tags+=("$root_tag" "$root_tag-cloud")
-done
-
-echo "FLUTTER $flutter_version from $base_tag"
-for version_tag in "3" "3.7" "$flutter_version"; do
-	root_tag="$tag_prefix:flutter-$version_tag"
-	docker build \
-		--file Flutter.Dockerfile \
-		--build-arg ANVIL_BASE_TAG=$base_tag \
-		--build-arg FLUTTER_VERSION=$flutter_version \
-		--tag $root_tag \
-		.docker_context/
-	docker build \
-		--file Cloud.Dockerfile \
-		--tag "$root_tag-cloud" \
-		--build-arg ANVIL_BASE_TAG=$root_tag \
-		.docker_context/
-	built_tags+=("$root_tag" "$root_tag-cloud")
-done
-
-for version_tag in "${built_tags[@]}"; do
-	echo "Pushing $version_tag"
-	docker push "$version_tag"
-done
